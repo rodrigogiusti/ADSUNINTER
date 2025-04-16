c87138d928674d864586f15b2154a6abdece1c5f
rodrigogiusti <rodrigo.escandura@gmail.com>
2025-04-16 12:11:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const botao = document.getElementById("botao");
+
+botao.addEventListener("click", function () {
+
+  const msg = document.getElementById("mensagem");
+  
+  msg.innerText = "ESTUDE MUITO TODOS OS DIAS E NÃO DESISTA!";
+});
